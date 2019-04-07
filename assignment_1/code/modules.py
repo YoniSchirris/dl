add backprop. now pass size tests. next up: fix the actual values

assignment_1/code/modules.py
@@ -28,8 +28,8 @@ class LinearModule(object):
     sigma = 0.0001
     
     # weights are R ^ out * in
-    weights = np.random.normal(mu, sigma, [out_features, in_features])
-    weights_grad = np.zeros([out_features, in_features])
+    weights = np.random.normal(mu, sigma, [in_features, out_features])
+    weights_grad = np.zeros([in_features, out_features])
 
     # biases are R ^ out
     biases = np.zeros(out_features)
@@ -64,8 +64,11 @@ class LinearModule(object):
 
     # W [out * in ] * x [in] + b [ out ]
 
-    out = np.dot(self.params['weights'], x) + self.params['bias']
 
+    out = np.dot(x, self.params['weight'])
+    out += self.params['bias']
+
+    self.x = x
     # END OF YOUR CODE    #
     #######################
 
@@ -87,9 +90,13 @@ class LinearModule(object):
 
     ########################
     # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
+    
+    W = self.params['weight']
+    dx = np.dot(dout, W.T)
+    # dx = np.einsum('k ,ij ->',dout, W)
+    dLdb = dout
+    dLdW = (np.dot(self.x, dx.T))
+
     # END OF YOUR CODE    #
     #######################
     
@@ -117,9 +124,13 @@ class ReLUModule(object):
     ########################
     # PUT YOUR CODE HERE  #
     
-    x[x<0]=0
+    # x[x<0]=0
+    #  out = x
 
-    out = x
+    self.I_tilde = (x>0)
+
+    out = x * self.I_tilde
+
 
     # END OF YOUR CODE    #
     #######################
@@ -141,9 +152,12 @@ class ReLUModule(object):
 
     ########################
     # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
+
+    I_tilde = self.I_tilde
+
+    dx = dout * I_tilde
+
+
     # END OF YOUR CODE    #
     #######################    
 
@@ -172,12 +186,9 @@ class SoftMaxModule(object):
     # PUT YOUR CODE HERE  #
 
     # as taken from https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
-    def exp_normalize(x):
-        b = x.max()
-        y = np.exp(x - b)
-        return y / y.sum()
-
-    out = exp_normalize(x)
+    y = np.exp(x - np.max(x))
+    out = y / np.sum(y)
+    self.out = out
     
     # END OF YOUR CODE    #
     #######################
@@ -199,9 +210,15 @@ class SoftMaxModule(object):
 
     ########################
     # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
+    xN = self.out
+
+    diag_holder = np.zeros((xN.shape[0], xN.shape[1], xN.shape[1]))
+    diag = np.arange(xN.shape[1])
+    diag_holder[:, diag, diag] = xN
+
+    dxdtilde = diag_holder - np.einsum('ij, ik -> ijk', xN, xN)
+
+    dx = np.einsum('ij, ijk -> ij', dout, dxdtilde)
     # END OF YOUR CODE    #
     #######################
 
@@ -228,7 +245,7 @@ class CrossEntropyModule(object):
     ########################
     # PUT YOUR CODE HERE  #
     
-    out = -1 * np.dot(y, np.log(x))
+    out = -1 * np.shape(np.sum(x*y, axis=1))
 
     # END OF YOUR CODE    #
     #######################
@@ -251,9 +268,9 @@ class CrossEntropyModule(object):
 
     ########################
     # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
+    
+    dx = -1 * np.divide(y, x)
+
     # END OF YOUR CODE    #
     #######################
 

assignment_1/code/train_mlp_numpy.py
@@ -45,9 +45,20 @@ def accuracy(predictions, targets):
 
   ########################
   # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
+
+  batch_size = np.shape(predictions)[0]
+  
+  max_value_of_prediction_per_image = np.max(predictions, 1)
+
+  predictions[predictions < max_value_of_prediction_per_image] = 0
+  predictions[predictions == max_value_of_prediction_per_image] = 1
+
+  # note to self: might have to check for duplicate predictions now
+  
+  correct_predictions = predictions*targets
+  number_of_correct_predictions = np.sum(correct_predictions)
+  accuracy = number_of_correct_predictions / batch_size
+
   # END OF YOUR CODE    #
   #######################
 
@@ -75,9 +86,50 @@ def train():
 
   ########################
   # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
+
+  # get test data to initialize the model with
+  cifar10 = cifar10_utils.get_cifar10(DATA_DIR_DEFAULT)
+
+  x_test, y_test = cifar10['test'].images, cifar10['test'].labels
+
+  input_size = np.shape(x_test)[1] * np.shape(x_test)[2] * np.shape(x_test)[3]
+  class_size = np.shape(y_test)[1]
+
+  # seems like it is still given as a string. So ensuring that it is a list of ints now
+
+  model = MLP(n_inputs=input_size, n_hidden=dnn_hidden_units, n_classes=class_size)
+
+  # vector_forward = np.vectorize(model.forward)
+  # vector_backward = np.vectorize(model.backward)
+
+  calculate_loss = CrossEntropyModule()
+  # vector_loss_forward = np.vectorize(calculate_loss.forward)
+  # vector_loss_backward = np.vectorize(calculate_loss.backward)
+
+  for step in range(MAX_STEPS_DEFAULT):
+        x, y = cifar10['train'].next_batch(BATCH_SIZE_DEFAULT)
+        x = x.reshape([np.shape(x)[0], input_size])
+        
+        # forward_out = vector_forward(x)
+        forward_out = model.forward(x)
+
+        #loss = vector_loss_forward(forward_out, y)
+        loss_gradient = calculate_loss.backward(forward_out, y)
+
+        model.backward(loss_gradient)
+
+        # evaluate every EVAL_FREQ_DEFAULT steps
+        if step % EVAL_FREQ_DEFAULT == 0:
+              # get the values for test input
+              test_forward = model.forward(x_test)
+              # print the accuracy
+              print(accuracy(test_forward, y_test))
+
+
+        
+
+
+
   # END OF YOUR CODE    #
   #######################
 