f914bf8a5d7215f781fa5ece95e7639e916b2c9e
Yoni Schirris <yoni@usabilla.com>
2019-04-10 21:19:08 +0200

    implement convnet pytorch

--- assignment_1/code/convnet_pytorch.py
+++ assignment_1/code/convnet_pytorch.py
@@ -6,54 +6,137 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+
 class ConvNet(nn.Module):
-  """
-  This class implements a Convolutional Neural Network in PyTorch.
-  It handles the different layers and parameters of the model.
-  Once initialized an ConvNet object can perform forward.
-  """
-
-  def __init__(self, n_channels, n_classes):
     """
-    Initializes ConvNet object. 
-    
-    Args:
-      n_channels: number of input channels
-      n_classes: number of classes of the classification problem
-                 
-    
-    TODO:
-    Implement initialization of the network.
+    This class implements a Convolutional Neural Network in PyTorch.
+    It handles the different layers and parameters of the model.
+    Once initialized an ConvNet object can perform forward.
     """
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+    def __init__(self, n_channels, n_classes):
+        """
+        Initializes ConvNet object.
 
-  def forward(self, x):
-    """
-    Performs forward pass of the input. Here an input tensor x is transformed through 
-    several layer transformations.
-    
-    Args:
-      x: input to the network
-    Returns:
-      out: outputs of the network
-    
-    TODO:
-    Implement forward pass of the network.
-    """
+        Args:
+          n_channels: number of input channels
+          n_classes: number of classes of the classification problem
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
 
-    return out
+        TODO:
+        Implement initialization of the network.
+        """
+
+        ########################
+        # PUT YOUR CODE HERE  #\
+
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0,
+        # dilation=1, groups=1, bias=True)
+
+        # torch.nn.MaxPool2d(kernel_size, stride=None, padding=0,
+        #                    dilation=1, return_indices=False, ceil_mode=False)
+
+        # avg pool:
+        # kernel_size: Any,
+        # stride: Any = None,
+        # padding: int = 0,
+        # ceil_mode: bool = False,
+        # count_include_pad: bool = True)
+
+        super(ConvNet, self).__init__()
+
+        # I assume we keep the std parameters for conv2d as dilation=1, groups=1, bias=True
+        # I assume we keep the std parameters for maxpool as dilation=1, return_indices=false, ceil_mode=false
+
+        self.feats = nn.Sequential(
+            # conv1
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            # maxpool1
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # conv2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            # maxpool2
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # conv3_a
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+
+            # conv3_b
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            # maxpool3
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # conv4_a
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            # conv4_b
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            # maxpool4
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # conv5_a
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            # conv5_b
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            # maxpool5
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # avgpool
+            nn.AvgPool2d(kernel_size=1, stride=1, padding=0),
+
+
+        )
+
+        self.linearLayer = nn.Linear(in_features=512, out_features=10)
+
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through
+        several layer transformations.
+
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+
+        TODO:
+        Implement forward pass of the network.
+        """
+
+        ########################
+        # PUT YOUR CODE HERE  #
+
+        out = self.feats(x)
+
+        # linear
+        out = self.linearLayer(out.view(x.shape[0], -1))
+
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
--- assignment_1/code/train_convnet_pytorch.py
+++ assignment_1/code/train_convnet_pytorch.py
@@ -12,6 +12,9 @@ import os
 from convnet_pytorch import ConvNet
 import cifar10_utils
 
+import torch
+from torch import optim
+
 # Default constants
 LEARNING_RATE_DEFAULT = 1e-4
 BATCH_SIZE_DEFAULT = 32
@@ -24,87 +27,137 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 
 FLAGS = None
 
+
 def accuracy(predictions, targets):
-  """
-  Computes the prediction accuracy, i.e. the average of correct predictions
-  of the network.
-  
-  Args:
-    predictions: 2D float array of size [batch_size, n_classes]
-    labels: 2D int array of size [batch_size, n_classes]
-            with one-hot encoding. Ground truth labels for
-            each sample in the batch
-  Returns:
-    accuracy: scalar float, the accuracy of predictions,
-              i.e. the average correct predictions over the whole batch
-  
-  TODO:
-  Implement accuracy computation.
-  """
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes]
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
 
-  return accuracy
+    TODO:
+    Implement accuracy computation.
+    """
+
+    ########################
+    # PUT YOUR CODE HERE  #
+    max_index_p = predictions.argmax(dim=1)
+    max_index_t = targets.argmax(dim=1)
+    accuracy = (max_index_p == max_index_t).float().mean().data.item()
+    # END OF YOUR CODE    #
+    #######################
+
+    return accuracy
+
 
 def train():
-  """
-  Performs training and evaluation of ConvNet model. 
+    """
+    Performs training and evaluation of ConvNet model.
 
-  TODO:
-  Implement training and evaluation of ConvNet model. Evaluate your model on the whole test set each eval_freq iterations.
-  """
+    TODO:
+    Implement training and evaluation of ConvNet model. Evaluate your model on the whole test set each eval_freq iterations.
+    """
 
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    ########################
+    # PUT YOUR CODE HERE  #
+
+
+    # check if GPU is available. If not, use CPU
+    if torch.cuda.is_available():
+        torch.device('cuda')
+    else:
+        torch.device('cpu')
+
+    cifar10 = cifar10_utils.get_cifar10(DATA_DIR_DEFAULT)
+
+    x_test, y_test = cifar10['test'].images, cifar10['test'].labels
+
+    num_channels = np.shape(x_test)[1]
+    class_size = np.shape(y_test)[1]
+
+    x_test = torch.from_numpy(x_test)
+    y_test = torch.from_numpy(y_test)
+
+    net = ConvNet(n_channels=num_channels, n_classes=class_size)
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE_DEFAULT)
+
+    for step in range(MAX_STEPS_DEFAULT):
+        print(step)
+        x, y = cifar10['train'].next_batch(BATCH_SIZE_DEFAULT)
+        x = torch.from_numpy(x)
+        y = torch.from_numpy(y)
+        optimizer.zero_grad()
+
+        out = net(x)
+
+        y = y.argmax(dim=1)
+
+        loss = criterion(out, y)
+        loss.backward()
+        optimizer.step()
+
+        print(loss.item())
+
+        if step % EVAL_FREQ_DEFAULT == 0:
+            # test_out = net.forward(x_test)
+            # print(accuracy(test_out, y_test))
+            test_out = net.forward(x)
+            print(accuracy(test_out, y))
+
+
+# END OF YOUR CODE    #
+    #######################
+
 
 def print_flags():
-  """
-  Prints all entries in FLAGS variable.
-  """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    """
+    Prints all entries in FLAGS variable.
+    """
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
 
 def main():
-  """
-  Main function
-  """
-  # Print all Flags to confirm parameter settings
-  print_flags()
+    """
+    Main function
+    """
+    # Print all Flags to confirm parameter settings
+    print_flags()
 
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
 
-  # Run the training operation
-  train()
+    # Run the training operation
+    train()
+
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
                         help='Frequency of evaluation on the test set')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
+    parser.add_argument('--data_dir', type=str, default=DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
 
-  main()
+    main()
