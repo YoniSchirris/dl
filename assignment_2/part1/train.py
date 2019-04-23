149d0e5ac2bded4f0cc7c3a2ebec3494778fc5a2
Yoni Schirris <yoni@usabilla.com>
2019-04-23 14:43:46 +0800

    write vanilla RNN forward. it runs

--- assignment_2/part1/train.py
+++ assignment_2/part1/train.py
@@ -30,10 +30,42 @@ from part1.dataset import PalindromeDataset
 from part1.vanilla_rnn import VanillaRNN
 from part1.lstm import LSTM
 
+
 # You may want to look into tensorboardX for logging
 # from tensorboardX import SummaryWriter
 
 ################################################################################
+
+def calculate_accuracy(predictions, targets):
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes]
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
+
+    TODO:
+    Implement accuracy computation.
+    """
+
+    ########################
+    # PUT YOUR CODE HERE  #
+
+    max_index_p = predictions.argmax(dim=1)
+    max_index_t = targets
+    accuracy = (max_index_p == max_index_t).float().mean().data.item()
+
+    # END OF YOUR CODE    #
+    #######################
+
+    return accuracy
+
 
 def train(config):
 
@@ -43,33 +75,55 @@ def train(config):
     device = torch.device(config.device)
 
     # Initialize the model that we are going to use
-    model = None  # fixme
+    # model = None  # fixme
+
+    SEQ_LENGTH= 5 # fixme
+    INPUT_DIM=config.input_dim
+    NUM_HIDDEN=config.num_hidden
+    NUM_CLASSES=config.num_classes
+    BATCH_SIZE=config.batch_size
+
+    model = VanillaRNN(seq_length=SEQ_LENGTH,input_dim=INPUT_DIM , num_hidden=NUM_HIDDEN, num_classes=NUM_CLASSES, batch_size=BATCH_SIZE, device='cpu')
 
     # Initialize the dataset and data loader (note the +1)
     dataset = PalindromeDataset(config.input_length+1)
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
     # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
+    # criterion = torch.  # fixme
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # optimizer = None  # fixme
+    optimizer = torch.optim.RMSprop(model.parameters())
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
         # Only for time measurement of step through network
         t1 = time.time()
 
+        # ----------------------------------------------------
         # Add more code here ...
 
         ############################################################################
         # QUESTION: what happens here and why?
+        # ANSWER: the gradients are clipped /rescaled to a max value, as explaind in slide 50 of lecture 6
         ############################################################################
-        torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
         ############################################################################
 
         # Add more code here ...
 
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
+        out = model.forward(batch_inputs)
+        optimizer.zero_grad()
+        loss = criterion(out, batch_targets)
+        loss.backward(retain_graph=True)
+        torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
+        optimizer.step()
+
+        accuracy = calculate_accuracy(out, batch_targets)
+        # loss = np.inf   # fixme
+        # accuracy = 0.0  # fixme
+
+
 
         # Just for time measurement
         t2 = time.time()
@@ -106,6 +160,7 @@ if __name__ == "__main__":
     parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
     parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
     parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
+    # parser.add_argument('--batch_size', type=int, default=64, help='Number of examples to process in a batch')
     parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--train_steps', type=int, default=10000, help='Number of training steps')
--- assignment_2/part1/vanilla_rnn.py
+++ assignment_2/part1/vanilla_rnn.py
@@ -21,14 +21,44 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+import numpy as np
+
+
 ################################################################################
 
 class VanillaRNN(nn.Module):
 
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(VanillaRNN, self).__init__()
-        # Initialization here ...
+
+        mu = 0
+        sigma = 0.0001
+
+        np.random.seed(42)
+
+        # initialize variables
+        self.Whx = nn.Parameter(torch.tensor(np.random.normal(mu, sigma, size=(num_classes, num_hidden))))
+
+        self.Whh = nn.Parameter(torch.tensor(np.random.normal(mu, sigma, size=(num_hidden, num_hidden))))
+
+        self.Wph = nn.Parameter(torch.tensor(np.random.normal(mu, sigma, size=(num_hidden, num_classes))))
+
+        self.bp = nn.Parameter(torch.tensor(np.random.normal(mu, sigma, size=(batch_size, num_classes))))
+
+        self.bh = nn.Parameter(torch.tensor(np.random.normal(mu, sigma, size=(batch_size, num_hidden))))
+
+        self.h = torch.zeros((batch_size, num_hidden))
+
+        self.seq_length = seq_length
+
+        self.num_hidden = num_hidden
 
     def forward(self, x):
-        # Implementation here ...
-        pass
+        for seq in range(self.seq_length):
+            a = x.double()@self.Whx.double()
+            b = self.h.double()@self.Whh.double()
+            c = self.bh
+            self.h = torch.tanh(a + b + c)
+
+        p = self.h@self.Wph + self.bp
+        return p
