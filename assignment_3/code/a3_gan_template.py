272bd89004bb775c69845f7956d244b744808128
Yoni Schirris <yoni@usabilla.com>
2019-05-12 15:22:21 +0200

    implementation of gan, runs properly

--- assignment_3/code/a3_gan_template.py
+++ assignment_3/code/a3_gan_template.py
@@ -7,10 +7,33 @@ import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torchvision import datasets
 
+import time
+
+
+#TODO add dropout during training
+
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
+
+        self.latent_dim = args.latent_dim
+
+        self.forward_pass = nn.Sequential(
+            nn.Linear(self.latent_dim, 128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
 
         # Construct generator. You are free to experiment with your model,
         # but the following is a good start:
@@ -29,13 +52,22 @@ class Generator(nn.Module):
         #   Output non-linearity
 
     def forward(self, z):
-        # Generate images from z
-        pass
+        out = self.forward_pass(z)
+        return out
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
+
+        self.forward_pass = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
         # Construct distriminator. You are free to experiment with your model,
         # but the following is a good start:
@@ -47,22 +79,118 @@ class Discriminator(nn.Module):
         #   Output non-linearity
 
     def forward(self, img):
-        # return discriminator score for img
-        pass
+        out = self.forward_pass(img)
+        return out
+
+# custom weights initialization called on netG and netD
+# as taken from pytorch tutorial: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+def sample(generator, device):
+    n = 25
+    z = torch.randn((n, generator.latent_dim), device=device)
+    Gz = generator(z)
+    save_image(Gz,
+               'images/{}-2-6.png'.format(str(int(time.time()))),
+               nrow=5, normalize=True)
 
 
-def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
+
+
+
+def train(dataloader, discriminator, generator, optimizer_G, optimizer_D, device):
+
+    stats = {}
+
+    stats['G'] = {}
+    stats['D'] = {}
+
+    stats['G']['loss'] = []
+    stats['G']['acc'] = []
+
+    stats['D']['loss'] = []
+    stats['D']['acc'] = []
+
+
     for epoch in range(args.n_epochs):
         for i, (imgs, _) in enumerate(dataloader):
 
-            imgs.cuda()
-
-            # Train Generator
-            # ---------------
+            x = imgs.to(device).reshape(-1, 784)
 
             # Train Discriminator
             # -------------------
             optimizer_D.zero_grad()
+
+            z = torch.randn((args.batch_size, args.latent_dim), device=device)
+
+            Dx = discriminator(x)
+            Gz = generator(z)
+            DGz2 = discriminator(Gz)
+
+            D_loss = -1 * (torch.log(Dx) + torch.log(1 - DGz2)).mean(dim=0)
+
+            D_loss.backward()
+
+            optimizer_D.step()
+
+            # Train Generator
+            # ---------------
+
+            for j in range(3):
+
+                z = torch.randn((args.batch_size, args.latent_dim), device=device)
+
+                DGz1 = discriminator(generator(z))
+
+                G_loss = (-1 * torch.log(DGz1)).mean(dim=0)
+
+                optimizer_G.zero_grad()
+
+                G_loss.backward()
+
+                optimizer_G.step()
+
+
+
+            # Save and print some stats
+            # ----
+
+            # we expect the generator accuracy to go from 0 to 0.5
+            # we expect the discriminator accuracy to go from 1 to 0.5
+            # we would like to plot the losses over time
+            # we would like to
+
+            generator_accuracy = (DGz1.mean(dim=0).item(), DGz2.mean(dim=0).item())
+            discriminator_accuracy = Dx.mean(dim=0).item()
+
+            GL = G_loss.item()
+            DL = D_loss.item()
+
+            stats['G']['acc'].append(generator_accuracy)
+            stats['G']['loss'].append(GL)
+
+            stats['D']['acc'].append(discriminator_accuracy)
+            stats['D']['loss'].append(DL)
+
+            print_per_steps = 100
+            if i % print_per_steps == 0:
+                to_print = "Epoch: {}\tStep: {}\tLoss_D: {}\tLoss_G: {}\tD(x): {}\tD(G(z)): {} / {}".format(
+                    epoch,
+                    i,
+                    DL,
+                    GL,
+                    discriminator_accuracy,
+                    generator_accuracy[0],
+                    generator_accuracy[1]
+                )
+
+                print(to_print)
 
             # Save Images
             # -----------
@@ -71,37 +199,56 @@ def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
                 # You can use the function save_image(Tensor (shape Bx1x28x28),
                 # filename, number of rows, normalize) to save the generated
                 # images, e.g.:
-                # save_image(gen_imgs[:25],
-                #            'images/{}.png'.format(batches_done),
-                #            nrow=5, normalize=True)
-                pass
+                # TODO Anything to change here?
+                Gz = Gz.reshape(-1, 1, 28, 28)
+                save_image(Gz[:25],
+                           'images/{}-{}.png'.format(str(int(time.time())) , batches_done),
+                           nrow=5, normalize=True)
 
 
-def main():
+def main(args):
+    # check if GPU is available. If not, use CPU
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    else:
+        device = torch.device('cpu')
+
+
     # Create output image directory
     os.makedirs('images', exist_ok=True)
 
-    # load data
+    # load data -- fixed the Normalize dimensions
     dataloader = torch.utils.data.DataLoader(
         datasets.MNIST('./data/mnist', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5),
-                                                (0.5, 0.5, 0.5))])),
+                           transforms.Normalize((0.5,),
+                                                (0.5,))])),
         batch_size=args.batch_size, shuffle=True)
 
     # Initialize models and optimizers
     generator = Generator()
     discriminator = Discriminator()
+
+    generator.to(device)
+    discriminator.to(device)
+
+    # reinitialize weights
+    generator.apply(weights_init)
+    discriminator.apply(weights_init)
+
+
+
     optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
 
     # Start training
-    train(dataloader, discriminator, generator, optimizer_G, optimizer_D)
+    train(dataloader, discriminator, generator, optimizer_G, optimizer_D, device)
 
     # You can save your generator here to re-use it to generate images for your
     # report, e.g.:
-    # torch.save(generator.state_dict(), "mnist_generator.pt")
+    torch.save(generator.state_dict(), str(int(time.time())) + "_mnist_generator.pt")
 
 
 if __name__ == "__main__":
@@ -118,4 +265,4 @@ if __name__ == "__main__":
                         help='save every SAVE_INTERVAL iterations')
     args = parser.parse_args()
 
-    main()
+    main(args)
